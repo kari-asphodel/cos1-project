c90e71ac7997ee92fdd15a8fd91dff116372d272
Kari Asphodel <124915307+kari-asphodel@users.noreply.github.com>
2026-09-09 14:27:08 -0400

    Added functions to taskmanager.cpp for completion, active and completed tasks

--- dev/TaskManager/TaskManager/TaskManager.cpp
+++ dev/TaskManager/TaskManager/TaskManager.cpp
@@ -3,25 +3,63 @@
 
 void TaskManager::AddTask(std::string title)
 {
-	tasks.push_back(Task(title));
+	activeTasks.push_back(Task(title));
 
 	std::cout << "\nTask added to the crypt successfully.\n";
 }
 
-void TaskManager::ViewTasks() const
+void TaskManager::ViewActiveTasks() const
 {
-	std::cout << "\n==== TASK CRYPT ====\n";
+	std::cout << "\n==== ACTIVE TASKS ====\n";
 
-	if (tasks.empty())
+	if (activeTasks.empty())
 	{
 		std::cout << "\nThe crypt is empty. No tasks haunt you... yet.\n";
 		return;
 	}
 
-	for (int i = 0; i < tasks.size(); i++)
+	for (int i = 0; i < activeTasks.size(); i++)
 	{
 		std::cout << i + 1 << ". "
-				  << tasks[i].GetTitle()
+				  << activeTasks[i].GetTitle()
 				  << "\n";
 	}
+}
+
+void TaskManager::ViewCompletedTasks() const
+{
+	std::cout << "\n==== COMPLETED TASKS ====\n";
+
+	if (completedTasks.empty())
+	{
+		std::cout << "\nNo completed tasks yet. The ghosts remain employed.\n";
+		return;
+	}
+
+	for (int i = 0; i < completedTasks.size(); i++)
+	{
+		std::cout << i + 1 << ". "
+			<< completedTasks[i].GetTitle()
+			<< "\n";
+	}
+}
+
+void TaskManager::CompleteTask(int index)
+{
+	if (index = > 0 && index < activeTasks.size())
+	{
+		activeTasks[index].CompleteTask();
+		completedTasks.push_back(activeTasks[index]);
+		activeTasks.erase(activeTasks.begin() + index);
+		std::cout << "\nTask completed and moved to the completed crypt.\n";
+	}
+	else
+	{
+		std::cout << "\nInvalid task number. That task does not exist in this realm.\n";
+	}
+}
+
+int TaskManager::GetActiveTaskCount() const
+{
+	return activeTasks.size();
 }
